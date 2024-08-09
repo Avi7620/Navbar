7f4aa4aa31c01e733fd733ae5c4d744dd4160b16
Avinash <jadhavavi7620@gmail.com>
2024-08-09 20:44:40 +0530

    Initial commit - setting up navigation bar

---
+++ navbar/script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger-menu").addEventListener("click", function () {
+    document.querySelector(".sidebar").classList.toggle("active");
+    document.querySelector(".content").classList.toggle("active");
+});
